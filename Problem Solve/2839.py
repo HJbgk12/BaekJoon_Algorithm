e4f9355186fc8927bb4bb93da4f47c06419baa46
HJbgk12 <rmfpdltm12363@gmail.com>
2022-01-14 20:54:42 +0900

    Create 2839.py
    https://www.acmicpc.net/problem/2839

---
+++ Problem Solve/2839.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(input())
+
+dp = [0 for i in range(5002)]
+dp[3] = 1
+dp[5] = 1
+
+for i in range(6,n+1):
+    if dp[i-3] == 0 and dp[i-5] == 0:
+        dp[i] = 0
+    elif dp[i-3] == 0:
+        dp[i] = dp[i-5]+1
+    elif dp[i-5] == 0:
+        dp[i] = dp[i-3]+1
+    else:
+        dp[i] = min(dp[i-3]+1, dp[i-5]+1)
+if dp[n]:
+    print(dp[n])
+else:
+    print("-1")
